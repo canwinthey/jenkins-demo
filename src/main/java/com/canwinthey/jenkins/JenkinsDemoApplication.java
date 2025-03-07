4f6badb58f4c762187cd9f72ef9c75adf8ae9776
Prasanna.Kanvinde <Prasanna.Kanvinde@Xoriant.com>
2025-03-07 13:21:04 +0530

    2nd commit

--- src/main/java/com/canwinthey/jenkins/JenkinsDemoApplication.java
+++ src/main/java/com/canwinthey/jenkins/JenkinsDemoApplication.java
@@ -9,6 +9,7 @@ public class JenkinsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 		System.out.println("Welcome to JenkinsDemoApplication!!");
+		System.out.println("Prasanna's 2nd edit");
 	}
 
 }
